4d24a57c3bc56d3335f515c53ee68b2afde4386e
Glen C <dontspamg@gmail.com>
2010-11-12 06:16:14 -0800

    adding options and un-obscuring

--- unique_vals.rb
+++ unique_vals.rb
@@ -2,8 +2,11 @@
 # Date  :  11/11/2010
 # Program Name: unique_vals.rb
 #   - takes input from a file and creates a report on which unique ids have unique values
+$usage = "USAGE: ruby unique_vals [-b] input_file \n"
+abort "#{$usage}try: ruby unique_vals --help (for more info)" if ARGV.size <= 0
+abort "-b     option will display bidder information for each auction\n--help opion dispays this message" if ARGV[0] == '--help'
+$theFile = ARGV[0].index('-').nil? ? ARGV[0] : ARGV[1]
 
-abort "USAGE: ruby unique_vals input_file" if ARGV.size <= 0
 
 #*******************************class Aggr(fileName)*******************************
 class Aggr
@@ -23,12 +26,25 @@ class Aggr
       lineArr = line.split(',')
       auct = lineArr[0]
       bidr = lineArr[1]
-      hash[auct].nil? ? hash[auct] = {bidr => 1} : hash[auct][bidr] = 1
+      # if auct doesn't exist we make it
+      if hash[auct].nil? 
+        hash[auct] = {bidr => 1} 
+      # if bidr doesn't exist we make it
+      elsif hash[auct][bidr].nil?
+        hash[auct][bidr] = 1
+      # add and iterate the existing auct / bidr
+      else 
+        hash[auct][bidr] += 1
+      end
     end
     file.close
     return hash
   end
 end # class Aggr
 
-mess = Aggr.new ARGV[0]
-mess.findUniqueLines.each {|k,v| puts "auction #{k} has #{v.size} unique bidders"}
+
+mess = Aggr.new $theFile
+mess.findUniqueLines.each do |k,v| 
+  puts "auction #{k} has #{v.size} unique bidders"
+  v.each {|b,t| puts "  bidder #{b} bid #{t} times"} if ARGV[0] == '-b'
+end
